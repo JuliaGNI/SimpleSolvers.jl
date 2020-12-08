f2832e9fe999b26a9e4d97a9bc2f7a82e18e3329
Documenter.jl <documenter@juliadocs.github.io>
2020-12-08 12:48:28 +0000

    build based on 95ab9af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleSolvers","category":"page"},{"location":"#SimpleSolvers","page":"Home","title":"SimpleSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleSolvers]","category":"page"}]
+}
