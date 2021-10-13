872f53a9c50eb2ab0b4a125fc3fa21ae69813187
Documenter.jl <documenter@juliadocs.github.io>
2021-10-13 21:29:40 +0000

    build based on b4b2713

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleSolvers","category":"page"},{"location":"#SimpleSolvers","page":"Home","title":"SimpleSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleSolvers]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleSolvers","category":"page"},{"location":"#SimpleSolvers","page":"Home","title":"SimpleSolvers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleSolvers]","category":"page"},{"location":"#SimpleSolvers.Armijo","page":"Home","title":"SimpleSolvers.Armijo","text":"simple Armijo line search\n\n\n\n\n\n","category":"type"}]
 }
