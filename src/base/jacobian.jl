1b29f2ac15826cc19c50e54df6096a8ee6daf7d2
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-11-30 11:10:49 +0100

    Allow Jacobian to be non-quadratic.

--- src/base/jacobian.jl
+++ src/base/jacobian.jl
@@ -14,20 +14,20 @@ struct JacobianParametersAD{T, FT <: Function, JT <: ForwardDiff.JacobianConfig}
     Jconfig::JT
     tx::Vector{T}
     ty::Vector{T}
+
+    function JacobianParametersAD(F!::FT, Jconfig::JT, tx::Vector{T}, ty::Vector{T}) where {T, FT, JT}
+        new{T, FT, JT}(F!, Jconfig, tx, ty)
+    end
 end
 
-function JacobianParametersAD(F!::FT, Jconfig::JT, tx::Vector{T}, ty::Vector{T}) where {T, FT, JT}
-    @assert length(tx) == length(ty)
-    JacobianParametersAD{T, FT, JT}(F!, Jconfig, tx, ty)
-end
-
-function JacobianParametersAD(F!::FT, T, n::Int) where {FT <: Function}
-    tx = zeros(T, n)
-    ty = zeros(T, n)
+function JacobianParametersAD(F!::FT, T, nx::Int, ny::Int) where {FT <: Function}
+    tx = zeros(T, nx)
+    ty = zeros(T, ny)
     Jconfig = ForwardDiff.JacobianConfig(F!, ty, tx)
-    Jparams = JacobianParametersAD(F!, Jconfig, tx, ty)
+    JacobianParametersAD(F!, Jconfig, tx, ty)
 end
 
+JacobianParametersAD(F!, T, n) = JacobianParametersAD(F!, T, n, n)
 
 struct JacobianParametersFD{T, FT} <: JacobianParameters{T}
     ϵ::T
@@ -38,22 +38,21 @@ struct JacobianParametersFD{T, FT} <: JacobianParameters{T}
     tx::Vector{T}
 end
 
-function JacobianParametersFD(F!::FT, ϵ, T, n::Int) where {FT <: Function}
-    f1 = zeros(T, n)
-    f2 = zeros(T, n)
-    e  = zeros(T, n)
-    tx = zeros(T, n)
+function JacobianParametersFD(F!::FT, ϵ, T, nx::Int, ny::Int) where {FT <: Function}
+    f1 = zeros(T, ny)
+    f2 = zeros(T, ny)
+    e  = zeros(T, nx)
+    tx = zeros(T, nx)
     Jparams = JacobianParametersFD{T, typeof(F!)}(ϵ, F!, f1, f2, e, tx)
 end
 
+JacobianParametersFD(F!, ϵ, T, n) = JacobianParametersFD(F!, ϵ, T, n, n)
 
 function computeJacobian(x::Vector{T}, J::Matrix{T}, params::JacobianParametersUser{T}) where {T}
     params.J(J, x)
 end
 
 function computeJacobian(x::Vector{T}, J::Matrix{T}, params::JacobianParametersFD{T}) where {T}
-    @assert length(x) == size(J, 1) == size(J, 2)
-
     local ϵⱼ::T
 
     for j in eachindex(x)
@@ -85,15 +84,17 @@ function computeJacobianAD(x::Vector{T}, J::Matrix{T}, F!::FT) where{T, FT <: Fu
 end
 
 
-function getJacobianParameters(J, F!, T, n)
+function getJacobianParameters(J, F!, T, nx, ny)
     if J === nothing
         if get_config(:jacobian_autodiff)
-            Jparams = JacobianParametersAD(F!, T, n)
+            Jparams = JacobianParametersAD(F!, T, nx, ny)
         else
-            Jparams = JacobianParametersFD(F!, get_config(:jacobian_fd_ϵ), T, n)
+            Jparams = JacobianParametersFD(F!, get_config(:jacobian_fd_ϵ), T, nx, ny)
         end
     else
         Jparams = JacobianParametersUser{T, typeof(J)}(J)
     end
     return Jparams
 end
+
+getJacobianParameters(J, F!, T, n) = getJacobianParameters(J, F!, T, n, n)
