c720a2e209fdaa7deb22b434238161805dc785d3
Michael Kraus <michael.kraus@ipp.mpg.de>
2021-12-01 15:34:10 +0100

    Minor fix.

--- src/optimization/quasi_newton_optimizer.jl
+++ src/optimization/quasi_newton_optimizer.jl
@@ -89,10 +89,10 @@ function solve!(s::QuasiNewtonOptimizer{T}; n::Int = 0) where {T}
             # compute residual
             residual!(s.status)
 
-            # println(s.status.i, ", ", s.x̄, ", ", s.ȳ, ", ", s.status.rₐ,", ",  s.status.rᵣ,", ",  s.status.rₛ)
+            # check for convergence
             if check_solver_converged(s.status, s.params) && s.status.i ≥ s.params.nmin && !(n > 0)
                 if s.params.nwarn > 0 && s.status.i ≥ s.params.nwarn
-                    println("WARNING: BFGS Optimizer took ", s.status.i, " iterations.")
+                    println("WARNING: Quasi-Newton Optimizer took ", s.status.i, " iterations.")
                 end
                 break
             end
