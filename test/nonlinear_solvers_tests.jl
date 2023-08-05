19884830469a63af81fc85e59c359a5b59a3ecdd
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-08-05 03:51:29 +0200

    Test nonlinear solvers with Float32.

--- test/nonlinear_solvers_tests.jl
+++ test/nonlinear_solvers_tests.jl
@@ -6,14 +6,6 @@ using Test
 
 struct NonlinearSolverTest{T} <: NonlinearSolver end
 
-test_solver = NonlinearSolverTest{Float64}()
-
-@test_throws ErrorException config(test_solver)
-@test_throws ErrorException status(test_solver)
-@test_throws ErrorException initialize!(test_solver, rand(3))
-@test_throws ErrorException solver_step!(test_solver)
-
-
 function F!(f, x)
     f .= x.^2
 end
@@ -26,37 +18,50 @@ function J!(g, x)
 end
 
 
-for (Solver, kwarguments) in (
-                (NewtonSolver, (linesearch = Static(),)),
-                (NewtonSolver, (linesearch = Backtracking(),)),
-                (NewtonSolver, (linesearch = Quadratic(),)),
-                (NewtonSolver, (linesearch = Bisection(),)),
-                (QuasiNewtonSolver, (linesearch = Static(),)),
-                (QuasiNewtonSolver, (linesearch = Backtracking(),)),
-                (QuasiNewtonSolver, (linesearch = Quadratic(),)),
-                (QuasiNewtonSolver, (linesearch = Bisection(),)),
-                # (NLsolveNewton, NamedTuple()),
-            )
+for DT in (Float32, Float64)
 
-    n = 1
-    x = ones(n)
-    y = zero(x)
-    nl = Solver(x, y; kwarguments...)
+    test_solver = NonlinearSolverTest{DT}()
 
-    @test config(nl) == nl.config
-    @test status(nl) == nl.status
+    @test_throws ErrorException config(test_solver)
+    @test_throws ErrorException status(test_solver)
+    @test_throws ErrorException initialize!(test_solver, rand(3))
+    @test_throws ErrorException solver_step!(test_solver)
 
-    solve!(x, F!, nl)
-    # println(status(nl))
-    for _x in x
-        @test _x ≈ 0 atol=1E-7
+
+    for (Solver, kwarguments) in (
+                    (NewtonSolver, (linesearch = Static(),)),
+                    (NewtonSolver, (linesearch = Backtracking(),)),
+                    (NewtonSolver, (linesearch = Quadratic(),)),
+                    (NewtonSolver, (linesearch = Bisection(),)),
+                    (QuasiNewtonSolver, (linesearch = Static(),)),
+                    (QuasiNewtonSolver, (linesearch = Backtracking(),)),
+                    (QuasiNewtonSolver, (linesearch = Quadratic(),)),
+                    (QuasiNewtonSolver, (linesearch = Bisection(),)),
+                    # (NLsolveNewton, NamedTuple()),
+                )
+
+        n = 1
+        x = ones(DT, n)
+        y = zero(x)
+        nl = Solver(x, y; kwarguments...)
+
+        @test config(nl) == nl.config
+        @test status(nl) == nl.status
+
+        solve!(x, F!, nl)
+        # println(status(nl))
+        for _x in x
+            @test _x ≈ 0 atol=sqrt(eps(DT))
+        end
+
+        x = ones(DT, n)
+        y = zero(x)
+        nl = Solver(x, y; J! = J!, kwarguments...)
+        solve!(x, F!, J!, nl)
+        # println(status(nl))
+        for _x in x
+            @test _x ≈ 0 atol=sqrt(eps(DT))
+        end
     end
 
-    x = ones(n)
-    nl = Solver(x, y; J! = J!, kwarguments...)
-    solve!(x, F!, J!, nl)
-    # println(status(nl))
-    for _x in x
-        @test _x ≈ 0 atol=1E-7
-    end
 end
