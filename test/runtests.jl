53c2ea1c0bda69c2b8aa3609de5b4c64dbb0d4a2
benedict-96 <benedikt.brantner@icloud.com>
2025-01-27 12:35:43 +0100

    Remove test from file names.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using SafeTestsets
 @safetestset "Hessians                                                                        " begin include("hessian_tests.jl") end
 @safetestset "Univariate objectives                                                           " begin include("univariate_objectives.jl") end
 @safetestset "Multivariate objectives                                                         " begin include("multivariate_objectives.jl") end
-@safetestset "Linear Solvers                                                                  " begin include("linear_solvers_tests.jl") end
-@safetestset "Line Searches                                                                   " begin include("line_searches_tests.jl") end
-@safetestset "Nonlinear Solvers                                                               " begin include("nonlinear_solvers_tests.jl") end
-@safetestset "Optimizers                                                                      " begin include("optimizers_tests.jl") end
+@safetestset "Linear Solvers                                                                  " begin include("linear_solvers.jl") end
+@safetestset "Line Searches                                                                   " begin include("line_searches.jl") end
+@safetestset "Nonlinear Solvers                                                               " begin include("nonlinear_solvers.jl") end
+@safetestset "Optimizers                                                                      " begin include("optimizers.jl") end
