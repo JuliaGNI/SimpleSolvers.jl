37a155a7b65455956ea5b5b281a46ff7b4a69a6f
benedict-96 <benedikt.brantner@icloud.com>
2025-01-29 18:24:36 +0100

    Added Newton structs to base methods (were falsely moved to linesearch methods).

--- src/base/methods.jl
+++ src/base/methods.jl
@@ -9,6 +9,10 @@ abstract type IterativeMethod <: LinearMethod end
 abstract type NewtonMethod <: NonlinearMethod end
 abstract type PicardMethod <: NonlinearMethod end
 
+struct Newton <: NewtonMethod end
+struct DFP <: NewtonMethod end
+struct BFGS <: NewtonMethod end
+
 Base.show(io::IO, alg::Newton) = print(io, "Newton")
 Base.show(io::IO, alg::DFP) = print(io, "DFP")
 Base.show(io::IO, alg::BFGS) = print(io, "BFGS")
