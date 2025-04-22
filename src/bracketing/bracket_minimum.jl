25c1b4b98fdb184166dc902fdfd59df49f28c11b
benedict-96 <benedikt.brantner@icloud.com>
2025-04-22 17:53:38 +0200

    Improved readibility of function bracket_minimum.

--- src/bracketing/bracket_minimum.jl
+++ src/bracketing/bracket_minimum.jl
@@ -1,7 +1,17 @@
-"Default constant."
+"""
+    const DEFAULT_BRACKETING_s
+
+Gives the default width of the interval (the bracket). See [`bracket_minimum`](@ref).
+"""
 const DEFAULT_BRACKETING_s = 1E-2
-"Default constant."
+
+"""
+    const DEFAULT_BRACKETING_k
+
+Gives the default ratio by which the bracket is increased if bracketing was not successful. See [`bracket_minimum`](@ref).
+"""
 const DEFAULT_BRACKETING_k = 2.0
+
 "Default constant"
 const DEFAULT_BRACKETING_nmax=100
 
@@ -13,21 +23,31 @@ const DEFAULT_BRACKETING_nmax=100
 - `k::`[`DEFAULT_BRACKETING_k`](@ref)
 - `nmax::`[`DEFAULT_BRACKETING_nmax`](@ref)
 """
-function bracket_minimum(f, x=0.0; s=DEFAULT_BRACKETING_s, k=DEFAULT_BRACKETING_k, nmax=DEFAULT_BRACKETING_nmax)
-    a, ya = x, f(x)
-    b, yb = a + s, f(a + s)
+function bracket_minimum(f::Callable, x::T=0.0; s::T=T(DEFAULT_BRACKETING_s), k::T=T(DEFAULT_BRACKETING_k), nmax::Integer=DEFAULT_BRACKETING_nmax)::Tuple{T, T} where {T <: Number}
+    a = x
+    ya = f(a)
+
+    b = a + s
+    yb = f(b)
+
+    # flip a & b if necessary
     if yb > ya
         a, b = b, a
         ya, yb = yb, ya
         s = -s
     end
+
     for _ in 1:nmax
-        c, yc = b + s, f(b + s)
-        # println("a=$a, b=$b, c=$c, f(a)=$ya, f(b)=$yb, f(c)=$yc")
+        c = b + s
+        yc = f(c)
         if yc > yb
-            return a < c ? (a,c) : (c,a)
+            interval = a < c ? (a, c) : (c, a)
+            return interval
         end
-        a, ya, b, yb = b, yb, c, yc
+        a = b
+        ya = yb
+        b = c
+        yb = yc
         s *= k
     end
     error("Unable to bracket f starting at x = $x.")
