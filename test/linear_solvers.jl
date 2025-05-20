c1140e8cbeab17304461c22678499fcf2e682e6e
benedict-96 <benedikt.brantner@icloud.com>
2025-05-20 22:13:04 +0200

    Fixed tests for linear solvers.

--- test/linear_solvers.jl
+++ test/linear_solvers.jl
@@ -9,13 +9,11 @@ test_solver = LinearSolverTest{Float64}()
 @test_throws ErrorException factorize!(test_solver)
 @test_throws ErrorException ldiv!(test_solver)
 
-
 A = [[+4.  +5.  -2.]
      [+7.  -1.  +2.]
      [+3.  +1.  +4.]]
 x = [+4., -4., +5.]
 b = [-14., +42., +28.]
-
 
 function test_lu_solver(solver, A, b, x)
     for T in (Float64, ComplexF64, Float32, ComplexF32)
@@ -23,12 +21,14 @@ function test_lu_solver(solver, A, b, x)
         bT = convert(Vector{T}, b)
         xT = convert(Vector{T}, x)
 
-        lu1 = solver(AT)
-        x1  = ldiv!(zero(xT), lu1, bT)
+        x1 = similar(xT)
+        ls1 = LinearSystem(x1, copy(AT), bT)
+        lu1 = solver(ls1)
+        # solve!(lu1)
         @test x1 ≈ xT atol=8*eps(real(T))
 
         lu2 = solver(rand(T, size(A)...))
-        x2  = zero(xT)
+        x2  = similar(xT)
         factorize!(lu2, AT)
         ldiv!(x2, lu2, bT)
         @test x2 ≈ xT atol=8*eps(real(T))
