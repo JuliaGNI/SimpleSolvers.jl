e8c351941718e6987a378151b7c2986a5a25d598
benedict-96 <benedikt.brantner@icloud.com>
2025-05-30 13:17:55 +0200

    Implemented __value and __derivative to dispatch on TemporaryUnivariateObjective to not display warnings.

--- src/linesearch/backtracking/backtracking.jl
+++ src/linesearch/backtracking/backtracking.jl
@@ -113,7 +113,7 @@ LinesearchState(algorithm::Backtracking; T::DataType = Float64, kwargs...) = Bac
 function (ls::BacktrackingState{T})(obj::AbstractUnivariateObjective{T}, α::T = ls.α₀) where {T}
     x₀ = zero(α)
     y₀ = __value!(obj, x₀)
-    d(α) = derivative!(obj, α)
+    d(α) = __derivative!(obj, α)
     d₀ = d(x₀)
 
     # note that we set pₖ ← 0 here as this is the descent direction for the linesearch objective.
@@ -130,5 +130,7 @@ function (ls::BacktrackingState{T})(obj::AbstractUnivariateObjective{T}, α::T =
     α
 end
 
-__value!(obj::UnivariateObjective, x₀) = value!(obj, x₀)
+__value!(obj::AbstractObjective, x₀) = value!(obj, x₀)
 __value!(obj::TemporaryUnivariateObjective, x₀) = value(obj, x₀)
+__derivative!(obj::AbstractObjective, x₀) = derivative!(obj, x₀)
+__derivative!(obj::TemporaryUnivariateObjective, x₀) = derivative(obj, x₀)
