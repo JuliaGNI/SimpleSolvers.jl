a6c35b78520c90426e4d3e9e50d3fc2a147b8020
benedict-96 <benedikt.brantner@icloud.com>
2025-11-07 14:01:36 +0100

    Introduced methods for nonlinear solvers (and had to also introduce one for optimizers in the process).

--- src/base/methods.jl
+++ src/base/methods.jl
@@ -4,15 +4,39 @@ abstract type LinearMethod <: SolverMethod end
 "A supertype collecting all nonlinear methods, including `NewtonMethod`s."
 abstract type NonlinearMethod <: SolverMethod end
 
-abstract type DirectMethod <: LinearMethod end
-abstract type IterativeMethod <: LinearMethod end
+abstract type NonlinearSolverMethod <: SolverMethod end
 
-abstract type NewtonMethod <: NonlinearMethod end
-abstract type PicardMethod <: NonlinearMethod end
+# abstract type DirectMethod <: LinearMethod end
+# abstract type IterativeMethod <: LinearMethod end
 
-struct Newton <: NewtonMethod end
-struct DFP <: NewtonMethod end
-struct BFGS <: NewtonMethod end
+"""
+    NewtonMethod()
+
+To be used together with [`NonlinearSolver`](@ref).
+"""
+struct NewtonMethod <: NonlinearSolverMethod end
+
+"""
+    QuasiNewtonMethod(refactorize)
+
+Make an instance of a *quasi Newton solver* based on an integer *refactorize* that determines how often the rhs is refactored.
+"""
+struct QuasiNewtonMethod <: NonlinearSolverMethod
+    refactorize::Int
+end
+
+"""
+    PicardMethod()
+
+Make an instance of a *Picard solver* (fixed point iterator).
+"""
+struct PicardMethod <: NonlinearSolverMethod end
+
+abstract type OptimizerMethod <: SolverMethod end
+
+struct Newton <: OptimizerMethod end
+struct DFP <: OptimizerMethod end
+struct BFGS <: OptimizerMethod end
 
 Base.show(io::IO, alg::Newton) = print(io, "Newton")
 Base.show(io::IO, alg::DFP) = print(io, "DFP")
