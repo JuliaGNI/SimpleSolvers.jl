13537462f875ca9914a36ef10896b7d2512fd5e5
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-12-10 20:59:43 +0100

    Fix another fusion issue.

--- src/linesearch/backtracking/condition.jl
+++ src/linesearch/backtracking/condition.jl
@@ -5,7 +5,7 @@ Abstract type comprising the conditions that are used for checking step sizes fo
 """
 abstract type BacktrackingCondition{T} end
 
-(bc::BCT, xₖ, αₖ, gradₖ) where {BCT <: BacktrackingCondition} = error("Condition $(BCT) not defined for this combination of input arguments.")
+(bc::BCT, xₖ, αₖ, gradₖ) where {BCT<:BacktrackingCondition} = error("Condition $(BCT) not defined for this combination of input arguments.")
 
 """
     compute_new_iterate!(xₖ₊₁, xₖ, αₖ, pₖ)
@@ -22,28 +22,28 @@ xₖ = xₖ + αₖ * pₖ
 
 For manifolds we instead perform a *retraction* [absil2008optimization](@cite).
 """
-function compute_new_iterate!(xₖ₊₁::VT, xₖ::VT, αₖ::T, pₖ::TVT) where {T, VT, TVT}
+function compute_new_iterate!(xₖ₊₁::VT, xₖ::VT, αₖ::T, pₖ::TVT) where {T,VT,TVT}
     error("Not implemented for $(VT).")
 end
 
-function compute_new_iterate!(xₖ₊₁::VT, xₖ::VT, αₖ::T, pₖ::TVT) where {T <: Number, VT <: Union{T, AbstractArray{T}}, TVT <: Union{T, AbstractArray{T}}}
-    xₖ₊₁ .= xₖ .+ αₖ * pₖ
+function compute_new_iterate!(xₖ₊₁::VT, xₖ::VT, αₖ::T, pₖ::TVT) where {T<:Number,VT<:Union{T,AbstractArray{T}},TVT<:Union{T,AbstractArray{T}}}
+    xₖ₊₁ .= xₖ .+ αₖ .* pₖ
 end
 
-function compute_new_iterate!(xₖ::VT, αₖ::T, pₖ::TVT) where {T <: Number, VT <: Union{T, AbstractArray{T}}, TVT <: Union{T, AbstractArray{T}}}
+function compute_new_iterate!(xₖ::VT, αₖ::T, pₖ::TVT) where {T<:Number,VT<:Union{T,AbstractArray{T}},TVT<:Union{T,AbstractArray{T}}}
     compute_new_iterate!(xₖ, xₖ, αₖ, pₖ)
 end
 
-function compute_new_iterate!(xₖ::VT, αₖ::T₁, pₖ::TVT) where {T₁ <: Number, T <: Number, VT <: Union{T, AbstractArray{T}}, TVT <: Union{T, AbstractArray{T}}}
+function compute_new_iterate!(xₖ::VT, αₖ::T₁, pₖ::TVT) where {T₁<:Number,T<:Number,VT<:Union{T,AbstractArray{T}},TVT<:Union{T,AbstractArray{T}}}
     @warn "Your are computing with mixed precisions ($(T₁) and $(T)). This is probably not on purpose."
     compute_new_iterate(xₖ, T(αₖ), pₖ)
 end
 
-function compute_new_iterate(xₖ::VT, αₖ::T, pₖ::TVT) where {T <: Number, VT <: Union{T, AbstractArray{T}}, TVT <: Union{T, AbstractArray{T}}}
+function compute_new_iterate(xₖ::VT, αₖ::T, pₖ::TVT) where {T<:Number,VT<:Union{T,AbstractArray{T}},TVT<:Union{T,AbstractArray{T}}}
     xₖ₊₁ = copy(xₖ)
     compute_new_iterate!(xₖ₊₁, αₖ, pₖ)
 end
 
-function compute_new_iterate(xₖ::T, αₖ::T, pₖ::T) where {T <: Number}
+function compute_new_iterate(xₖ::T, αₖ::T, pₖ::T) where {T<:Number}
     xₖ + αₖ * pₖ
 end
