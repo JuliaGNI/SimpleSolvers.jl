5066013564acaf679d750908f88e8ac615416a9e
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-12-10 15:07:34 +0100

    Update benchmark and profile scripts.

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,36 +1,33 @@
 using SimpleSolvers
 
 
-function F!(f, x)
-    f .= x.^2
+function F!(f, x, params)
+    f .= x .^ 2
     nothing
 end
 
 
 function test(n)
     for (Solver, kwarguments) in (
-                    (NewtonSolver, (linesearch = Static(),)),
-                    (NewtonSolver, (linesearch = Backtracking(),)),
-                    (NewtonSolver, (linesearch = Quadratic(),)),
-                    (NewtonSolver, (linesearch = Bisection(),)),
-                    (QuasiNewtonSolver, (linesearch = Static(),)),
-                    (QuasiNewtonSolver, (linesearch = Backtracking(),)),
-                    (QuasiNewtonSolver, (linesearch = Quadratic(),)),
-                    (QuasiNewtonSolver, (linesearch = Bisection(),)),
-                )
+        (NewtonSolver, (linesearch=Static(),)),
+        (NewtonSolver, (linesearch=Backtracking(),)),
+        (NewtonSolver, (linesearch=Quadratic2(),)),
+        (NewtonSolver, (linesearch=BierlaireQuadratic(),)),
+        (NewtonSolver, (linesearch=Bisection(),)),
+    )
 
         for T in (Float64, Float32)
             x = ones(T, n)
             y = zero(x)
-            nl = Solver(x, y; kwarguments...)
+            nl = Solver(x, y; F=F!, kwarguments...)
 
             println(Solver, ", ", kwarguments, ", ", T)
 
             x = ones(T, n)
-            solve!(x, F!, nl)
+            solve!(nl, x)
 
             x = ones(T, n)
-            @time solve!(x, F!, nl)
+            @time solve!(nl, x)
 
             println()
         end
