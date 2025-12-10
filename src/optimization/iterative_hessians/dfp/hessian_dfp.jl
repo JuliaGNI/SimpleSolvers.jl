36df6a23f3b0170dbeda4d36210cbdd4d43c7a0c
benedict-96 <benedikt.brantner@icloud.com>
2025-12-10 12:08:17 +0100

    Added type.

--- src/optimization/iterative_hessians/dfp/hessian_dfp.jl
+++ src/optimization/iterative_hessians/dfp/hessian_dfp.jl
@@ -4,9 +4,9 @@
 The [`Hessian`](@ref) corresponding to the [`DFP`](@ref) method.
 """
 struct HessianDFP{T,FT <: Callable} <: IterativeHessian{T}
-    F
+    F::FT
 
-    function HessianDFP(F::FT, x::AbstractVector{T}) where {T, FT <: Callable}
+    function HessianDFP(F::FT, ::AbstractVector{T}) where {T, FT <: Callable}
         new{T, FT}(F)
     end
 end
