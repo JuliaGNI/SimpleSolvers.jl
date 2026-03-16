c1528eed58f0aa845d5c6b0cd52064ab648d4fb4
benedict-96 <benedikt.brantner@icloud.com>
2026-03-16 11:46:09 +0100

    It seems `clear!` routine for `OptimizerResult` is not needed (this is only generated at the end of the optimization process).

--- src/optimization/optimizer_result.jl
+++ src/optimization/optimizer_result.jl
@@ -15,19 +15,3 @@ status(result::OptimizerResult) = result.status
 
 solution(result::OptimizerResult) = result.x
 Base.minimum(result::OptimizerResult) = result.f
-
-"""
-    clear!(result)
-
-Clear all the information contained in `result::`[`OptimizerResult`](@ref).
-This also calls [`clear!(::OptimizerStatus)`](@ref).
-
-!!! info
-    Calling `initialize!` on an `OptimizerResult` calls `clear!` internally.
-"""
-function clear!(result::OptimizerResult{XT,YT}) where {XT,YT}
-    result.x .= XT(NaN)
-    result.f = YT(NaN)
-
-    result
-end
